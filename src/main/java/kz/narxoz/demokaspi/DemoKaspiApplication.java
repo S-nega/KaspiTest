a75018eeadc2e689862e7dc3804bea334a2bd6d3
s-nega <snezhana.golovko@narxoz.kz>
2022-11-12 10:49:18 +0600

    initial springboot commit

---
+++ src/main/java/kz/narxoz/demokaspi/DemoKaspiApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.demokaspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKaspiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoKaspiApplication.class, args);
+    }
+
+}
