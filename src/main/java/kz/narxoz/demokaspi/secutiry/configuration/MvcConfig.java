36860fced09d5cbd157294223fc87746a09236d9
s-nega <snezhana.golovko@narxoz.kz>
2022-12-07 17:34:09 +0600

    fixed security

---
+++ src/main/java/kz/narxoz/demokaspi/secutiry/configuration/MvcConfig.java
@@ -0,0 +1,10 @@
+//package kz.narxoz.demokaspi.secutiry.configuration;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class MvcConfig {
+//
+//
+//}
---
+++ src/main/java/kz/narxoz/demokaspi/secutiry/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+//package kz.narxoz.demokaspi.secutiry.configuration;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+//}
